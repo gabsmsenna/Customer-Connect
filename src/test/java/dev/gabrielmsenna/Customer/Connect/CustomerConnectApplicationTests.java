c3c877b129b86c04c0bcab06409ba93a7ebfe737
Gabriel <gabrielmagalhaes3344@gmail.com>
2025-01-06 21:47:19 -0300

    Iniciado projeto

---
+++ src/test/java/dev/gabrielmsenna/Customer/Connect/CustomerConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielmsenna.Customer.Connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
